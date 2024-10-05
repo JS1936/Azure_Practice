a4b882b8afc8cb53b2cbf655775bfac095998ffd
JS1936 <102188643+JS1936@users.noreply.github.com>
2024-10-04 18:09:19 -0700

    now use port 8000. photo_remote.py

--- photo_remote.py
+++ photo_remote.py
@@ -50,4 +50,5 @@ def upload_file():
         return f"Photo uploaded successfully: {file.filename}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000)
