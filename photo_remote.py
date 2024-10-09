bf0d4991d8e505bf8ec943840a9b91eb3d0e4a5a
JS1936 <102188643+JS1936@users.noreply.github.com>
2024-10-09 05:47:30 -0700

    change port to 8080 from 8000 -  photo_remote.py

--- photo_remote.py
+++ photo_remote.py
@@ -50,5 +50,5 @@ def upload_file():
         return f"Photo uploaded successfully: {file.filename}"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8080)
     #app.run(debug=True)
